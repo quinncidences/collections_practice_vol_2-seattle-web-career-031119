615bb31b5c0a8a33e60ccd6591421e0bcf978688
Quinn Cox <qrc.inpa@gmail.com>
2019-03-29 23:06:49 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,38 @@
-# your code goes here
+require 'pry'
+def begins_with_r(array)
+  array.all? { |word| word.start_with?("r") }
+end
+
+def contain_a(array)
+  array.select {|word| word.match("a")}
+end
+
+def first_wa(array)
+  array.detect {|word| word[0,2] == "wa"}
+end
+
+def remove_non_strings(array)
+  array.delete_if {|word| !word.is_a?(String)}
+end
+
+def count_elements(array)
+    array.uniq.each {|name| count = 0
+   
+      array.each {|person| if person == name then count += 1 
+      end}
+      # binding.pry
+      name[:count] = count}
+end
+
+def merge_data(keys, values)
+  # keys.each do |info|
+  #   keyvalues = info[:first_name]
+  #   valueinfo = info[0][keyvalues]
+    
+  #   valueinfo.each do |property,value|
+  #     object[property] = value
+  #   end
+  # end
+  
+  
+end
